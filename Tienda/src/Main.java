df5deedb52a343e5e46e186b80264fb836b8ae88
gines <ggutierrezs01@iescastelar.com>
2024-02-14 14:09:47 +0100

    na

--- Tienda/src/Main.java
+++ Tienda/src/Main.java
@@ -2,15 +2,12 @@ import productos.*;
 import java.util.Scanner;
 public class Main {
     private static Scanner scr = new Scanner(System.in).useDelimiter("\\n");
-
     public static void main(String[] args) {
         Producto[] tienda = new Producto[10]; //el maximo de productos es 10, por lo que se crea array con 10 productos del tipo productos, la superclase
 
         tienda[Producto.getInventario()] = new Movil(scr.next(),scr.next(),scr.nextFloat(), scr.next(), scr.nextInt(),scr.nextInt());
         tienda[Producto.getInventario()] = new Ordenador("asus","rog",1000,"core17",16,1000);
         tienda[Producto.getInventario()] = new Televisor("LG","Casa",400,32);
-
-        //for (Producto producto: tienda //usar cuando se que lo voy a utilzar en un array que tiene comienzo y final (es decir, está completo)) { System.out.println(producto)}
 
         int option;
         do {
@@ -33,20 +30,16 @@ public class Main {
                     productosTipo(scr.nextInt(), tienda);
                     break;
                 case 5:
-                    System.out.println("Por favor introducir Marca y Modelo");
-//                    consultarPrecio(scr.next(),scr.next(),tienda);
                     consultarPrecio2(tienda);
                     break;
                 case 6:
-                    //llamar(scr.next(),scr.next(),tienda);
-                    llamar2(tienda);
+                    llamar(tienda);
                     break;
                 case 7:
-                    System.out.println("Por favor introducir Marca y Modelo");
-                    aumentarRam(scr.next(),scr.next(),tienda);
+                    aumentarRam(tienda);
                     break;
                 case 8:
-                    cambiarCanal(scr.next(),scr.next(),tienda);
+                    camiarCanal(tienda);
                     break;
             }
         }while (option!=0);
@@ -123,8 +116,8 @@ public class Main {
 
     public static int buscarProducto(String marca, String modelo,Producto[] tienda){
         for (int i = 0; i<Producto.getInventario();i++){
-            if (modelo.equals(tienda[i].getMarca()) & marca.equals(tienda[i].getModelo())){
-                return -i;
+            if (modelo.equals(tienda[i].getMarca()) &&  marca.equals(tienda[i].getModelo())){
+                return i;
             }
         }System.out.println("No existe ningún producto con marca: "+marca+" y modelo: "+modelo);
         return -1;
@@ -136,49 +129,35 @@ public class Main {
             System.out.println(tienda[poscionArray].calcularDescuento());
         }
     }
-    public static String marcaPreguntar(){System.out.println("Introduir MARCA: "); return scr.next();}
-    public static String modeloPreguntar(){ System.out.println("Introduir MODELO: "); return scr.next();}
-
-
-    public static void llamar2(Producto[] tienda){
-        int poscionArray = buscarProducto(marcaPreguntar(),modeloPreguntar(),tienda);
-
-        if (poscionArray != -1) System.out.println("Introducir número: "); ((Movil)tienda[poscionArray]).llamar(scr.next());
-    }
-
-
-
+    private static String marcaPreguntar(){System.out.println("Introduir MARCA: "); return scr.next();}
+    private static String modeloPreguntar(){ System.out.println("Introduir MODELO: "); return scr.next();}
 
     //LLAMAR
-    public static void llamar(String marca, String modelo, Producto[] tienda) {
-        for (int i = 0; i < Producto.getInventario(); i++) {
-            if ((modelo.equals(tienda[i].getMarca())) & (marca.equals(tienda[i].getModelo()))) {
-                if (tienda[i] instanceof Movil) ((Movil) tienda[i]).llamar(scr.next());
-                return;
-            }
-        }System.out.println("No existe ningún producto con marca: " + marca + " y modelo: " + modelo);
+    public static void llamar(Producto[] tienda){
+        int poscionArray = buscarProducto(marcaPreguntar(),modeloPreguntar(),tienda);
+        if (poscionArray != -1) {
+            System.out.println("Introducir número: ");
+            ((Movil)tienda[poscionArray]).llamar(scr.next());
+        }
+        else System.out.println("Marca y/o Modelo incorrectos");
     }
     //CAMBIAR CANAL
-    public static void cambiarCanal(String marca, String modelo, Producto[] tienda) {
-        Scanner scr = new Scanner(System.in);
-
-        for (int i = 0; i < Producto.getInventario(); i++) {
-            if ((modelo.equals(tienda[i].getMarca())) & (marca.equals(tienda[i].getModelo()))) {
-                if (tienda[i] instanceof Televisor) ((Televisor) tienda[i]).cambiarCanal(scr.next());
-                return;
-            }
-        } System.out.println("No existe ningún producto con marca: " + marca + " y modelo: " + modelo);
+    public static void camiarCanal(Producto[] tienda){
+        int poscionArray = buscarProducto(marcaPreguntar(),modeloPreguntar(),tienda);
+        if (tienda[poscionArray] instanceof Televisor){
+            System.out.println("Introducir canal: ");
+            ((Televisor)tienda[poscionArray]).cambiarCanal(scr.next());
+        }
+        else System.out.println("Marca y/o Modelo incorrectos");
     }
     //AUMENTAR RAM
-    public static void aumentarRam(String marca, String modelo, Producto[] tienda) {
-        Scanner scr = new Scanner(System.in);
-
-        for (int i = 0; i < Producto.getInventario(); i++) {
-            if ((modelo.equals(tienda[i].getMarca())) & (marca.equals(tienda[i].getModelo()))) {
-                if (tienda[i] instanceof Ordenador) ((Ordenador) tienda[i]).ampliarRam(scr.nextInt());
-                return;
-            }
-        }System.out.println("No existe ningún producto con marca: "+marca+" y modelo: "+modelo);
+    public static void aumentarRam(Producto[] tienda){
+        int poscionArray = buscarProducto(marcaPreguntar(),modeloPreguntar(),tienda);
+        if (tienda[poscionArray] instanceof Ordenador){
+            System.out.println("RAM a añadir: ");
+            ((Ordenador)tienda[poscionArray]).ampliarRam(scr.nextInt());
+        }
+        else System.out.println("Marca y/o Modelo incorrectos");
     }
     //MENU
     public static void menu(){
@@ -193,16 +172,3 @@ public class Main {
                 "\n[8] CAMBIAR DE CANAL");
     }
 }
-
-
-/*
-NO SE PUEDE HACER PORQUE NEXTLINE NO FUNCIONA CORRECCTAMENTE
-
- System.out.print("Introduce los datos del móvil (\nMarca:" + "\nModelo" + "\nPrecio" + "\nSistemaOperativo" + "\nCapacidadAlmacenamiento" + "\nRAM): \n");
- String movilCrear = scr.nextLine();
- String partes[] = movilCrear.split(" ");
- if (partes.length !=6){
- System.out.println("Por facot introducir las Características necesarias [6]");
- } else tienda[Producto.getInventario()] = new Movil(partes[0], partes[1], Float.parseFloat(partes[2]), partes[3], Integer.parseInt(partes[4]), Integer.parseInt(partes[5]));
-
- */
