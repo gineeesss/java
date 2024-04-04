76cc6ab75bf5944cb7443caa2327c7a1ea1952af
gines <ggutierrezs01@iescastelar.com>
2024-04-04 14:13:15 +0200

    da

--- Mi primera coleccion/src/Main.java
+++ Mi primera coleccion/src/Main.java
@@ -1,13 +1,11 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static Scanner scr = new Scanner(System.in).useDelimiter("\\n");
     public static void main(String[] args) {
         //TIPO(Lista(Objeto)<tipo> nombre_var = valor(new Objeto(constructor);
         List<Integer> numeros = new ArrayList<>(3);
-        int opcion = 0;
+        int opcion;
         do{
             menu();
             opcion= scr.nextInt();
@@ -17,16 +15,15 @@ public class Main {
                     numeros.add(scr.nextInt());
                     break;
                 case 2:
-                    System.out.println("Lista Original: ");
-                    for (Integer a: numeros) {
-                        System.out.println(a);
-                    }
+                    mostrarLista(numeros);
                     break;
                 case 3:
                     break;
                 case 4:
                     break;
                 case 5:
+                    Set<Integer> conjuntoSinRepetir = new HashSet<>(numeros);
+                    mostrar(conjuntoSinRepetir);
                     break;
                 case 6:
                     break;
@@ -38,14 +35,33 @@ public class Main {
         System.out.println("\n[0] Salir del programa" +
                 "\n[1] Añadir Número" +
                 "\n[2] Mostrar la serie original de números que haya" +
-                "\n[3] Mostrar la serie ordenada de mayor a menor");
+                "\n[5] Mostrar la serie de números pero sin que haya números repetidos");
+    }
+    public static void mostrarLista(Collection<Integer> numeros){
+        System.out.println("Lista Original: ");
+        for (Integer a: numeros) {
+            System.out.print(a+" ");
+        }
+    }
+    public static <T> void mostrar(Collection<T> numeros){
+        System.out.println("Lista Original: ");
+        for (T a: numeros) {
+            System.out.print(a+" ");
+        }
     }
 }
-/*  Añadir números
 
-    Mostrar la serie ordenada de menor a mayor
 
-    Mostrar la serie de números pero sin que haya números repetidos
+
+
+
+
+
+
+
+
+/*  Mostrar la serie ordenada de menor a mayor
+    Mostrar la serie ordenada de mayor a menor
     Quitar un número de la lista indicando la posición
     Quitar un número de la lista indicando el número
     Quitar los números pares
