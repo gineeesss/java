bcb113dece6c1272426698da8e42f1bab9fd0842
gines <ggutierrezs01@iescastelar.com>
2024-04-05 12:29:10 +0200

    asd

--- Mi primera coleccion/src/Main.java
+++ Mi primera coleccion/src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         //TIPO(Lista(Objeto)<tipo> nombre_var = valor(new Objeto(constructor);
         List<Integer> numeros = new ArrayList<>(3);
+        ArrayList<Integer> cosita = new ArrayList<>();
+        ArrayList<Integer> numerosOrdenados;
         int opcion;
         do{
             menu();
@@ -13,21 +15,50 @@ public class Main {
                 case 1:
                     System.out.println("Siguiente Número Lista: ");
                     numeros.add(scr.nextInt());
-                    break;
+                break;
                 case 2:
-                    mostrarLista(numeros);
+                    mostrarColeccion(numeros);
                     break;
                 case 3:
+                    //mostrarColeccion(new ArrayList<Integer>(numeros));
+                    numerosOrdenados = new ArrayList<>(numeros);
+                    numerosOrdenados.sort(null);
+                    //lista.clear();
+                    //lista.addAll(numeros);
+                    Collections.sort(numerosOrdenados);
+                    mostrarColeccion(numerosOrdenados);
                     break;
                 case 4:
+                    numerosOrdenados = new ArrayList<>(numeros);
+                    numerosOrdenados.sort(Comparator.reverseOrder()); //sort requiere de un comparador, por lo que lo ideal
+                    //es usar un metodo que devuelva un comparador --> interfaz Comparator
+                    mostrarColeccion(numerosOrdenados);
                     break;
                 case 5:
-                    Set<Integer> conjuntoSinRepetir = new HashSet<>(numeros);
-                    mostrar(conjuntoSinRepetir);
+                    //Set<Integer> conjuntoSinRepetir = new HashSet<>(numeros);
+                    //mostrar(conjuntoSinRepetir);
+                    mostrarColeccion(new HashSet<>(numeros));
                     break;
                 case 6:
+                    System.out.println("Posición a borrar: ");
+                    cosita.add(numeros.remove(scr.nextInt()));
+                    mostrarLista(cosita);
                     break;
-
+                case 7:
+                    int borrar = scr.nextInt();
+                    Iterator<Integer> iterador = numeros.iterator();
+                    while(iterador.hasNext()){
+                        if(iterador.next()==borrar) iterador.remove();
+                    }
+                    break;
+                case 9:
+                    /*int suma=0;
+                    for(int i=0;i<numeros.size();i++){
+                        suma+=numeros.get(i);
+                    }
+                    System.out.println("La suma de todos los números de la Colección es: "+ suma);*/
+                    sumarColeccion(numeros);
+                    break;
             }
         }while(opcion!=0);
     }
@@ -35,7 +66,12 @@ public class Main {
         System.out.println("\n[0] Salir del programa" +
                 "\n[1] Añadir Número" +
                 "\n[2] Mostrar la serie original de números que haya" +
-                "\n[5] Mostrar la serie de números pero sin que haya números repetidos");
+                "\n[3] Mostrar la serie ordenada de menor a mayor" +
+                "\n[4] Mostrar la serie ordenada de mayor a menor" +
+                "\n[5] Mostrar la serie de números pero sin que haya números repetidos" +
+                "\n[6] Quitar un número de la lista indicando la posición" +
+                "\n[7] Quitar un número de la lista indicando el número" +
+                "\n[9] Sumar todos los elementos");
     }
     public static void mostrarLista(Collection<Integer> numeros){
         System.out.println("Lista Original: ");
@@ -43,26 +79,26 @@ public class Main {
             System.out.print(a+" ");
         }
     }
-    public static <T> void mostrar(Collection<T> numeros){
+    public static <T> void mostrarColeccion(Collection<T> numeros){
         System.out.println("Lista Original: ");
         for (T a: numeros) {
             System.out.print(a+" ");
         }
     }
+    public static void sumarColeccion(Collection<Integer> numeros){
+        int suma=0;
+        for (Integer a: numeros) {
+            suma += a;
+        }
+        System.out.println("La suma de todos los números de la Colección es: "+ suma);
+    }
 }
 
 
 
+/*
 
 
 
-
-
-
-
-/*  Mostrar la serie ordenada de menor a mayor
-    Mostrar la serie ordenada de mayor a menor
-    Quitar un número de la lista indicando la posición
-    Quitar un número de la lista indicando el número
     Quitar los números pares
     Suma todos los números de la lista*/
