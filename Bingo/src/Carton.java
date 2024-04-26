078abea82247a62c43db3e2899bf4c061c505054
gines <ggutierrezs01@iescastelar.com>
2024-04-26 12:37:04 +0200

    adds

---
+++ Bingo/src/Carton.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+/*import java.util.HashSet;
+import java.util.Set;
+
+public class Carton {
+    public Set<Integer> carton = new HashSet<>(5);
+
+    public Carton() {
+        int i=0;
+        while (i<5){
+            if (carton.add((int)(Math.random()*20+1))) i++;
+        }
+    }
+
+    public void mostrarBolas(){
+        for (Integer a: carton){
+            System.out.print("("+a.toString()+") ");
+        }
+    }
+}
+*/
--- Conjunto1/src/Main.java
+++ Conjunto1/src/Main.java
@@ -1,19 +1,37 @@
-import java.util.LinkedHashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     static Scanner scr = new Scanner(System.in).useDelimiter("\\n");
     public static void main(String[] args) {
-        Set<String> diccionario = new LinkedHashSet<>();
+        Set<String> diccionario = new LinkedHashSet<>(0);
         String aniadir;
-        do{
+        aniadir="";
+        while(!aniadir.equalsIgnoreCase("fin")){
+            System.out.print("Introducir palabra posición "+diccionario.size()+": ");
             aniadir= scr.next();
-            if (!diccionario.add(aniadir)) {
+            if (!diccionario.add(aniadir.toLowerCase())) {
                 System.out.println("La palabra "+aniadir+" ya existe en el diccionario.");
             }
-        }while(!aniadir.equalsIgnoreCase("fin"));
+        }
         System.out.println(diccionario);
+        System.out.println(diccionario);
+        /* CONJUNTO ORDENADO DE MANERA PERUANA
+        List<String> dicionarioLista = new ArrayList(diccionario);
+        Collections.sort(dicionarioLista);
+        Set<String> conjuntoOrdenado = new LinkedHashSet<>(dicionarioLista);
+        System.out.println(conjuntoOrdenado);
+        */
+        Set<String> diccionarioOrdenado = new TreeSet<>(diccionario);
+        System.out.println(diccionarioOrdenado);
+
+        //Mejor manera, como solo se usa una vez
+        System.out.println(new TreeSet<>(diccionario));
+
+
+        //Conjunto ordenado por tamaño de palara < a >
+        Set<String> otroDiccionario = new TreeSet<>(new OrdenTamañoParole());
+        otroDiccionario.addAll(diccionario);
+        System.out.println(otroDiccionario);
     }
 }
 
