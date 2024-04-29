79d3fc16fc6e88f2da7ffbbd5d00b0a3ea03e957
gines <ggutierrezs01@iescastelar.com>
2024-04-29 12:23:52 +0200

    asd

--- Bingo/src/Main.java
+++ Bingo/src/Main.java
@@ -7,22 +7,58 @@ public class Main {
     public static void main(String[] args) {
         List<Set<Integer>> cartones = new ArrayList<>(3);
         Set<Integer> carton;
-        for (int j=0;j<3;j++){
-            carton= new HashSet<>(5);
+        int jugadores =3, tamCarton=5;
+        for (int j=0;j<jugadores;j++){
+            carton= new HashSet<>(tamCarton);
             cartones.add(carton);
+            // cartones.add(Set.copyOf(carton)); // para hacer una copia del valor del objeto
             int i=0;
-            while (i<5){
+            while (i<tamCarton){
                 if (carton.add((int)(Math.random()*20+1))) i++;
             }
         }
 
-        //System.out.println(cartones);
-        for (int i=0;i<3;i++){
+        for (int i=0;i<jugadores;i++){
             System.out.print("Carton nº"+(i+1)+": ");
             System.out.print(cartones.get(i));
             carton =cartones.get(i);
-            System.out.println(carton.toArray());
+
+            //System.out.println(carton.toArray());
             System.out.println();
+        }/*
+        for (int i = 0; i<jugadores;i++){
+            cartones.add(new HashSet<>(tamCarton));
+            int j = 0;
+            while (cartones.get(i).size()!=tamCarton){
+                cartones.get(j).add(numAleatorio());
+            }
+            System.out.print("Carton nº"+(i+1)+": ");
+            System.out.print(cartones.get(i));
+            System.out.println();
+        }*/
+        Set<Integer> bombo = new HashSet<>(20);
+        int j = 0;
+        System.out.println();
+        boolean ganador = false;
+        while(!ganador){
+            int numero = numAleatorio();
+            if(bombo.add(numero)){
+                System.out.print("("+numero+") ");
+                // System.out.println(bombo);
+                //System.out.println();
+                for(Set a: cartones){
+                    if (bombo.containsAll(a) && bombo.size()>=tamCarton){
+                        System.out.println();
+                        System.out.println("\nGanador carton: "+(cartones.indexOf(a)+1));
+                        ganador=true;
+                    }
+                }
+                j++;
+            }
         }
+        System.out.println(bombo);
+    }
+    public static int numAleatorio(){
+        return (int)(Math.random()*20+1);
     }
 }
